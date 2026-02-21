1f92a8b04594ad2b3852649327cbab7b54225bc4
babywolf <fgsoftwarestudio@gmail.com>
2026-02-21 17:06:01 +0000

    - framebuffer

--- run.fish
+++ run.fish
@@ -1,3 +1,4 @@
 #!/bin/fish
 
-qemu-system-i386 -cdrom wolfieOS.iso -m 512M -display gtk,gl=on,zoom-to-fit=on
+qemu-system-i386 -cdrom wolfieOS.iso -m 512M -display gtk,gl=on,zoom-to-fit=on \
+    #-debugcon file:debug.log -d int,cpu_reset -D qemu.log -no-reboot
